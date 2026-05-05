5e9029215845153e56f93eb64e63d4d3763b2ccd
Diya Patel <pdiya232007@gmail.com>
2026-05-05 14:42:20 +0530

    Initial upload of data scripts

---
+++ data.py
@@ -0,0 +1,29 @@
+import csv
+
+with open('sales_data.csv', mode='r') as file:
+    reader = csv.DictReader(file)
+    
+    product_totals = {}
+
+    for row in reader:
+        try:
+
+            product = row['product']
+            quantity = int(row['quantity'])
+            price = float(row['price'])
+        
+
+            revenue = quantity * price
+        
+            if product in product_totals:
+             product_totals[product] += revenue
+            else:
+             product_totals[product] = revenue
+        except ValueError:
+            print(f"Skipping a row because of missing or invalid data for: {row['product']}")
+            continue
+
+
+print("Total Revenue Per Product:")
+for product, total in product_totals.items():
+    print(f"{product}: {total:,.2f}")
