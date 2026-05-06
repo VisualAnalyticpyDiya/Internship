e8e531ad009e5583febc1b1d0d6bc9166ee566b9
Diya Patel <pdiya232007@gmail.com>
2026-05-06 17:37:54 +0530

    Add files via upload

--- data.py
+++ data.py
@@ -22,8 +22,19 @@ with open('sales_data.csv', mode='r') as file:
         except ValueError:
             print(f"Skipping a row because of missing or invalid data for: {row['product']}")
             continue
+top_product = ""
+max_rev = 0
+
+for product, total in product_totals.items():
+    if total > max_rev:
+        max_rev = total
+        top_product = product
 
 
 print("Total Revenue Per Product:")
 for product, total in product_totals.items():
     print(f"{product}: {total:,.2f}")
+
+
+print("-" * 30)
+print(f"Top-Selling Product: {top_product} (${max_rev:,.2f})")
