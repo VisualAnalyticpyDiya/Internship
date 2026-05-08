338d8e0906dcc23b5ed689c4e476f445b498085a
Diya Patel <pdiya232007@gmail.com>
2026-05-08 14:29:40 +0530

    Add files via upload

--- data.py
+++ data.py
@@ -1,40 +1,30 @@
 import csv
-
-with open('sales_data.csv', mode='r') as file:
+with open('sales_data.csv', mode='r')as file:
     reader = csv.DictReader(file)
-    
-    product_totals = {}
-
+    product_total={}
     for row in reader:
         try:
-
             product = row['product']
             quantity = int(row['quantity'])
             price = float(row['price'])
-        
-
-            revenue = quantity * price
-        
-            if product in product_totals:
-             product_totals[product] += revenue
+            rev = quantity * price
+            if product in product_total:
+                product_total[product] += rev
             else:
-             product_totals[product] = revenue
+                product_total[product]=rev
         except ValueError:
-            print(f"Skipping a row because of missing or invalid data for: {row['product']}")
-            continue
-top_product = ""
-max_rev = 0
-
-for product, total in product_totals.items():
-    if total > max_rev:
-        max_rev = total
-        top_product = product
-
-
-print("Total Revenue Per Product:")
-for product, total in product_totals.items():
-    print(f"{product}: {total:,.2f}")
-
-
-print("-" * 30)
-print(f"Top-Selling Product: {top_product} (${max_rev:,.2f})")
+                print(f"Skipping a row because of missing or invalid data for: {row['product']}")
+                continue
+top_product=""
+max_rev=0
+        
+for product, total in product_total.items():
+             if total > max_rev:
+                max_rev=total
+                top_product=product
+                print("totalm revenue per product:")
+                for product , total in product_total.items():
+                     print(f"{product}: {total:,.2f}")
+                     print("-"*30)
+                     print(f"top-selling product: {top_product} (${max_rev:,.2f})")
+                     
